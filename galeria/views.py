006163d529b1e29c4548e6bf8f0abd2c295ee49f
DanielDeus89 <danielsilvadeus@gmail.com>
2024-12-05 01:23:05 -0300

    Utilizando SQL

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+        1:{'nome':'Nebulosa de Carina','legenda':'webbtelecope / NASA / James webb'},
+        2:{'nome':'Galáxia NGC 1079','legenda':'nasa.org / NASA / Hublle'}
+    }
+
+    return render(request, 'galeria/index.html', {'cards':dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
